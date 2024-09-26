41a422d334604690d1ff8cf2d3c3377f3ad30087
ndtuananh04 <125666050+ndtuananh04@users.noreply.github.com>
2024-09-26 16:07:02 +0700

    update blog page

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,26 @@
         slidesToScroll: 1,
         dots: true,
         prevArrow: '<button type="button" class="slick-prev custom-arrow">●</button>',
-        nextArrow: '<button type="button" class="slick-next custom-arrow">●</button>'
+        nextArrow: '<button type="button" class="slick-next custom-arrow">●</button>',
+            responsive: [
+      {
+        breakpoint: 1024,   // At 1024px screen width
+        settings: {
+          slidesToShow: 2,  // Show 2 slides
+          slidesToScroll: 1 // Scroll 1 at a time
+        }
+      },
+      {
+        breakpoint: 768,    // At 768px screen width
+        settings: {
+          slidesToShow: 1,  // Show 1 slide
+          slidesToScroll: 1 // Scroll 1 at a time
+        }
+      }
+    ]
       });
+    
+    $('header__icon').click(function(){
+        
+    });
 })(jQuery, window);
